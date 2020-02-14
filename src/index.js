9c6cb77bfd25028a1b12ac455c87e80a944365ad
Jenny <18839970925@163.com>
2020-02-14 21:51:14 +0800

    feat: 回滚

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// 这种方式可以理解为创建了一个简单的react元素
-// const app = <h1>hello react!!</h1>
-// const createApp = props => {
-//   return (
-//     <div>
-//       {/* 只要在jsx中输入js代码就在外层加入一层花括号，注释也是js，所以这里也加入了一层花括号 */}
-//       <h1>hello {props.title}!</h1>
-//       <p>React 是一个用于构建用户界面的 JavaScript 库</p>
-//     </div>
-//   )
-// }
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// const app = createApp({
-//   title: 'react world '
-// })
-
-//创建组件的第一种方式，就是使用箭头函数，组件名要大写
-const App = props => {
-  return (
-    <div>
-      {/* 只要在jsx中输入js代码就在外层加入一层花括号，注释也是js，所以这里也加入了一层花括号 */}
-      <h1 title={props.title}>hello {props.title}!</h1>
-      <p>React 是一个用于构建用户界面的 JavaScript 库</p>
-    </div>
-  )
-}
-
-ReactDom.render(<App title="react"/>, document.querySelector('#root'))
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
