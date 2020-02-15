b08545318834b6c1fcac14d193d6b85da0591b7a
Jenny <18839970925@163.com>
2020-02-15 13:31:37 +0800

    feat: 组件中的样式

--- src/index.js
+++ src/index.js
@@ -1,63 +1,27 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import classNames from 'classnames'
+import styled from 'styled-components'
 
-// JSX原理 表示一个虚拟dom树的方式
-// const appVDom = {
-//   tag: 'div',
-//   attrs: {
-//     className: 'app',
-//     id: 'appRoot'
-//   },
-//   children: [
-//     {
-//       tag: 'h1',
-//       attrs: {
-//         className: 'title'
-//       },
-//       children: ['JSX原理']
-//     },
-//     {
-//       tag: 'p',
-//       attrs: null,
-//       children: ['类组件是继承React.Component的']
-//     }
-//   ]
-// }
+import './index.css'
 
-// 这里是使用类的形式创建的组件，这是jsx的语法，但不是合法的js
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="app" id="appRoot">
-//         <h1 className="title">JSX原理</h1>
-//         <p>类组件是继承React.Component的</p>
-//       </div>
-//     )
-//   }
-// }
+const Title = styled.p`
+ color: gray;
+`
 
-// 所以react在真正渲染的时候会把上面的代码编译成下面这种形式来运行，下面的是合法的js
 class App extends Component {
   render() {
-    // React.createElement是一个方法，用于创建元素，可以有很多的参数，但是前两个是固定的
-    // 第一个是标签名
-    // 第二个是标签的属性
-    // 剩下的就是更多的子元素
-    // React.createElement(type, [props], [...children])
-    return React.createElement(
-      'div',
-      {
-        className: 'app',
-        id: 'appRoot'
-      },
-      React.createElement(
-        'h1',
-        {
-          className: 'title'
-        },
-        'JSX原理'
-      ),
-      React.createElement('p', null, '类组件是继承React.Component的')
+    const style = {color: 'red'}
+    return (
+      <div>
+        <h1>元素中的样式</h1>
+        <ul>
+          <li style={style}>使用style内联创建</li>
+          <li className="has-text">使用class的方式，但是在react中class要写成className</li>
+          <li className={classNames('a',{'b': true, 'c': false})}>要动态添加不同的className就可以使用第三方的包叫classnames，比如这个li标签上就只有a b 没有c</li>
+          <li><Title>styled-components的使用</Title></li>
+        </ul>
+      </div>
     )
   }
 }
