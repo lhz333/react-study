6f5a387d9eb3e40922b0544132623675eef4be02
Jenny <18839970925@163.com>
2020-02-16 11:45:56 +0800

    feat: 事件

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import {
-  TodoHeader,
-  TodoInput,
-  TodoList,
-  Like
-} from './components'
+import { TodoHeader, TodoInput, TodoList, Like } from './components'
 
 import './index.css'
 
@@ -20,18 +15,50 @@ export default class App extends Component {
       title: '待办事项列表',
       desc: <i>今日事今日毕</i>,
       artice: '<div><h1>文章标题</h1><i>作者</i></div>',
-      todos: [{
-        id: 1,
-        title: '吃饭',
-        assignee: 'jenny',
-        isCompleted: true
-      },{
-        id: 2,
-        title: '睡觉',
-        assignee: 'nicai',
-        isCompleted: false
-      }]
+      todos: [
+        {
+          id: 1,
+          title: '吃饭',
+          assignee: 'jenny',
+          isCompleted: true
+        },
+        {
+          id: 2,
+          title: '睡觉',
+          assignee: 'nicai',
+          isCompleted: false
+        }
+      ]
     }
+  }
+  // 添加待办事项列表
+  addTodo = todoTitle => {
+    // console.log(todoTitle)
+    this.setState({
+      // 1、不能直接使用push添加数据，因为push返回的是数组的长度
+      // todos: this.state.todos.push({
+      //   id: Math.random(),
+      //   title: todoTitle,
+      //   isCompleted: false
+      // })
+      //2、使用这种方式添加新数据
+      // todos: this.state.todos.concat({
+      //   id: Math.random(),
+      //   title: todoTitle,
+      //   isCompleted: false
+      // })
+    })
+
+    // 如果想使用push 可以换一种方式 
+    const todoNews = [...this.state.todos]
+    todoNews.push({
+      id: Math.random(),
+      title: todoTitle,
+      isCompleted: false
+    })
+    this.setState({
+      todos: todoNews
+    })
   }
   render() {
     return (
@@ -47,11 +74,11 @@ export default class App extends Component {
             return <div key={todo.id}>{todo.title}</div>
           })
         } */}
-        <TodoHeader title={this.state.title} x='1' y='2' n={2} m={3}>
+        <TodoHeader title={this.state.title} x="1" y="2" n={2} m={3}>
           {this.state.desc}
         </TodoHeader>
-        <TodoInput btnText="ADD"/>
-        <TodoList todos={this.state.todos}/>
+        <TodoInput btnText="ADD" todo={this.addTodo} />
+        <TodoList todos={this.state.todos} />
         <h1>组件</h1>
         <Like></Like>
       </Fragment>
--- src/components/TodoInput/index.js
+++ src/components/TodoInput/index.js
@@ -10,11 +10,54 @@ export default class TodoInput extends Component {
   static defaultProps = {
     btnText: '添加'
   }
+  // 构造函数只执行一次
+  constructor() {
+    super()
+    this.state = {
+      inputValue: ''
+    }
+  }
+  handleInputChange = e => {
+    this.setState({
+      inputValue: e.currentTarget.value
+    })
+  }
+  // 2、不使用箭头函数时，需要bind(this)
+  // handleAddClick(value) {
+  //   console.log(this.state, value)
+  // }
+
+  // 3、父子组件传参
+  handleAddClick = () => {
+    if (!this.state.inputValue) return
+    this.props.todo(this.state.inputValue)
+    this.setState({
+      inputValue: ''
+    })
+  }
+
   render() {
     return (
       <div>
-        <input type="text" />
-        <button>{this.props.btnText}</button>
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+        />
+        {/* 1、事件的第一种写法，这时是一个匿名函数，并且写在了render渲染中，这样每次渲染就会生成一个匿名函数，这时我们就会把方法提到外面去 */}
+        {/* <button
+          onClick={() => {
+            console.log(this.state)
+          }}
+        >
+          {this.props.btnText}
+        </button> */}
+
+        {/* 2、使用bind(this, value)传参 */}
+        {/* <button onClick={this.handleAddClick.bind(this, '参数')}>{this.props.btnText}</button> */}
+
+        {/* 3 */}
+        <button onClick={this.handleAddClick}>{this.props.btnText}</button>
       </div>
     )
   }
